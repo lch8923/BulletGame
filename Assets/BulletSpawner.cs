913c9da47a1f902ac6c4e484583bc98615036e6d
lch8923 <66227715+lch8923@users.noreply.github.com>
2020-06-19 11:25:05 +0900

    6/19이창훈1차

---
+++ Assets/BulletSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bulletPrefab; 
+    public float spawnRateMin = 0.5f; 
+    public float spawnRateMax = 3f;   
+    private Transform target;          
+    private float spawnRate;          
+    private float timeAfterSpawn;      
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
